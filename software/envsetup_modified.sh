31efced867ddec2de5702a68ffa7b1aaad233383
gaheadus <gaheadus@126.com>
2021-02-27 21:13:32 +0800

    envsetup.sh

---
+++ software/envsetup_modified.sh
@@ -0,0 +1,23 @@
+export PATH="/opt/hisi-linux/x86-arm/arm-himix100-linux/bin:$PATH"
+
+function cgrep()
+{
+    find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
+        -exec grep --color -n "$@" {} +
+}
+
+function cgrepi()
+{
+    find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
+        -exec grep --color -n -i "$@" {} +
+}
+
+function agrep()
+{
+    find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f -exec grep --color -n "$@" {} +
+}
+
+function agrepi()
+{
+    find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f -exec grep --color -n -i "$@" {} +
+}
