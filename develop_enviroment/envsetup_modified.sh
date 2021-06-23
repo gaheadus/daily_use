c37a83ee64c1aad70ebc00ae7b7b7bda014d9a52
gaheadus <gaheadus@126.com>
2021-06-23 20:27:19 +0800

    modify sourceinsight config

--- develop_enviroment/envsetup_modified.sh
+++ develop_enviroment/envsetup_modified.sh
@@ -1,27 +1,39 @@
 function cgrep()
 {
-    find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
+    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name *.o -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
         -exec grep --color -n "$@" {} +
+}
+
+function cgrepe()
+{
+    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name *.o -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
+        -exec grep --color -n -E "$@" {} +
 }
 
 function cgrepi()
 {
-    find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
+    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name *.o -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
         -exec grep --color -n -i "$@" {} +
+}
+
+function cgrepei()
+{
+    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name *.o -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
+        -exec grep --color -n -E -i "$@" {} +
 }
 
 function agrep()
 {
-    find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f -exec grep --color -n "$@" {} +
+    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name *.o -prune -o -type f -exec grep --color -n "$@" {} +
 }
 
 function agrepi()
 {
-    find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f -exec grep --color -n -i "$@" {} +
+    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name *.o -prune -o -type f -exec grep --color -n -i "$@" {} +
 }
 
 function nocgrep()
 {
-    find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -name '*.c' -prune -o -name '*.cc' -prune -o -name '*.cpp' -prune -o -name '*.h' -prune -o -name '*.hpp' -prune -o -type f \
+    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name *.o -prune -o -name '*.c' -prune -o -name '*.cc' -prune -o -name '*.cpp' -prune -o -name '*.h' -prune -o -name '*.hpp' -prune -o -type f \
         -exec grep --color -n "$@" {} +
 }
