161e8de72e413e73237c508e6da803198b0d3c42
gaheadus <gaheadus@126.com>
2021-06-28 22:03:21 +0800

    update for envsetup.sh

--- develop_enviroment/envsetup_modified.sh
+++ develop_enviroment/envsetup_modified.sh
@@ -1,25 +1,15 @@
+cat <<EOF
+使用方法
+$ cgrep thename
+$ cgrep -i thename
+$ cgrep -E “\<thename\>”
+$ cgrep -i -E “\<thename\>”
+EOF
+
 function cgrep()
 {
     find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name *.o -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
         -exec grep --color -n "$@" {} +
-}
-
-function cgrepe()
-{
-    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name *.o -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
-        -exec grep --color -n -E "$@" {} +
-}
-
-function cgrepi()
-{
-    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name *.o -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
-        -exec grep --color -n -i "$@" {} +
-}
-
-function cgrepei()
-{
-    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name *.o -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
-        -exec grep --color -n -E -i "$@" {} +
 }
 
 function agrep()
@@ -27,13 +17,9 @@ function agrep()
     find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name *.o -prune -o -type f -exec grep --color -n "$@" {} +
 }
 
-function agrepi()
-{
-    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name *.o -prune -o -type f -exec grep --color -n -i "$@" {} +
-}
-
 function nocgrep()
 {
     find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name *.o -prune -o -name '*.c' -prune -o -name '*.cc' -prune -o -name '*.cpp' -prune -o -name '*.h' -prune -o -name '*.hpp' -prune -o -type f \
         -exec grep --color -n "$@" {} +
 }
+
