f3e7965e16026abd10d2370cb9a6db9af3565ff7
gaheadus <gaheadus@163.com>
2021-08-10 21:53:24 +0800

    修改envsetup.sh

--- develop_enviroment/envsetup_modified.sh
+++ develop_enviroment/envsetup_modified.sh
@@ -1,9 +1,11 @@
 cat <<EOF
-使用方法
+说明：这里的函数定义只在原有命令的基础上增加了一些过滤条件，保持原有grep、find的语法，使用方法和原有grep、find一样。
+例：
 $ cgrep thename
 $ cgrep -i thename
 $ cgrep -E “\<thename\>”
 $ cgrep -i -E “\<thename\>”
+$ ffind -name thename -type f
 EOF
 
 function cgrep()
@@ -27,4 +29,9 @@ function mgrep()
 {
     find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name '*.o' -prune -o \( -regextype posix-egrep -iregex '(.*\/Makefile|.*\/Makefile\..*|.*\.make|.*\.mak|.*\.mk|.*\.bp)' -o -regextype posix-extended -regex '(.*/)?soong/[^/]*.go' \) -type f \
         -exec grep --color -n "$@" {} +
+}
+
+function ffind()
+{
+    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o "$@" -print
 }
