4546822dbacf943455cbc6a7f7a96c1cffbc1cec
dukang <gaheadus@126.com>
2022-05-23 20:31:21 +0800

    新增生词

---
+++ English/word_list_1.c
@@ -0,0 +1,34 @@
+/* 英语单词，来源于微软必应词典、百度翻译。
+author: dukang, gaheadus@126.com, 20220516  */
+
+
+lebal:
+约定：
+单词写成C语言'函数'或'标签'的形式。写成'函数'好看一些。
+单词名写成函数名，读音、释义、例句等都放到单引号或双引号里面，例'example'、"example 2"。放双引号里看起来舒服些。
+文字外面加双引号放在任何地方都可以，"this is an example 3"，整体就是一个字符串。
+注意：读音重音符号用ˈ，不用单引号'，否则sourceinsight不能正确解析函数定义'。
+优点：
+1)极大的方便单词查找，方便学习。通过sourceinsight的符号窗口快速定位到单词所在位置。word文档、记事本等不能搜索目录文字，不能快速定位。
+2)sourceinsight将函数名加粗突出显示，方便阅读。
+3)通过符号窗口搜索，可以找出相近单词、重复单词。
+4)例句里面的生词，可以在附近位置写成函数调用，这样可以快速跳转到单词释义所在位置，结合sourceinsight快捷键前进后退用起来非常舒服，例如:verdict();
+非常好用，这个方法值得推广。
+如果朗文词典/牛津词典能按照这个方式编写也不错，比一页一页翻着查找速度快多了，哈哈哈。这样能在词典里做笔记，并不影响词典原有内容，不错。
+
+
+
+
+easymesh_related:
+如下是easymesh相关单词，主要来自协议标准文档。
+
+
+verdict(){ " 美 [ˈvɜrdɪkt]
+英 [ˈvɜː(r)dɪkt]
+n.裁决；结论；判决；判断；裁定；(经过检验或认真考虑后的)决定，结论，意见
+1.（陪审团的）裁定，裁决，裁断
+   a decision that is made by a jury in court, stating if sb is considered guilty of a crime or not
+2. ~ (on sth/sb) （经过检验或认真考虑后的）决定，结论，意见
+   a decision that you make or an opinion that you give about sth, after you have tested it or considered it carefully
+The judge will delay his verdict until he receives medical reports on the offender.法官将推迟判决，直到收到有关违法者的医疗报告。 "}
+
