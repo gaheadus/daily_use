f1115a9ae351c9477a4dfc027df96f6f09c5c311
dukang <gaheadus@126.com>
2022-07-04 22:12:33 +0800

    update 20220704

--- English/word_list_1.c
+++ English/word_list_1.c
@@ -40,3 +40,91 @@ n.锻压；锻造厂；(思想等的)锻炼；铁工厂
 3.[t] ~ sth (from sth) 锻造；制作 to shape metal by heating it in a fire and hitting it with a hammer; to make an object in this way
 4.[i] + adv./prep. 稳步前进 to move forward in a steady but powerful way
 Strategic alliances are being forged with major European companies.正与欧洲主要公司设法结成战略同盟。//艰苦干成 "}
+
+coarse(){" US [kɔrs] UK [kɔː(r)s]
+adj.粗糙的；粗织的；粗的；大颗粒的  Web粗劣的；粗俗的；粗调
+Coarse grain 粗粮                " }
+
+perimeter(){"US [pəˈrɪmɪtər] UK [pəˈrɪmɪtə(r)]
+n.边界；界线；【数】周长  Web周界；周边；视野计    "}
+
+schematic(){"US [skiˈmætɪk] UK [skiːˈmætɪk]
+adj.略图的；简表的；严谨的；有章法的
+n.简图
+Web原理图；电路图；示意
+The main thing is to show how Christianity did not develop in the smooth schematic way that it's presented in the Book of Acts.
+我主要是想你们回答，基督教是如何，没有按照《使徒行传》中所列的，平稳发展方式发展的。
+The new out of a small demo, which includes the circuit schematic diagram and demo program, to share with you.
+新出的小demo，其中包括电路原理图和demo程序，与大家分享一下。                                                                    "}
+
+couple(){" US [ˈkʌp(ə)l] UK ['kʌp(ə)l]
+n.情侣；两人；两件事物；几个人
+v.结合；性交；交配
+Web夫妇；一对；力偶
+1.[singspv] ~ (of sth) 两人；两件事物two people or things
+2.[singspv] ~ (of sth) 几个人；几件事物a small number of people or things
+3.[cspv] （人）一对；（尤指）夫妻，情侣two people who are seen together, especially if they are married or in a romantic or sexual relationship
+couple with v.连接；结合 Web伴随；与…相结合；与……结合
+Several extension modules are included with the driver. Some modules
+are tightly coupled with the core driver, linked into the same kernel module in Linux.                                      "}
+
+spawn(){" US [spɔn] UK [spɔːn]
+v.产卵；导致；引发；引起
+n.（鱼、蛙等的）卵
+Web再生侠；闪灵悍将；产生
+1.[i][t]  ~ (sth) 产卵to lay eggs
+2.[t] ~ sth 引发；引起；导致；造成to cause sth to develop or be produced
+Some drivers spawn separate threads to deal with devices that require polling or to deal with specialized timeout conditions.
+有一些驱动程序产生一些独立的辅助线程来处理那些需要轮询访问或处理超时的设备。                                                  "}
+
+collocate(){" US [ˈkʌləkət] UK [ˈkɒləkət]
+v.【语】词语的组合；排列；搭配;连用
+n.【语】经常搭配的词；搭配词，组合词
+Web并列；并置；配置
+Market is the best efficient mechanism to collocate knowledge resource.
+配置知识资源，市场机制还是最有效的方式。                                 "}
+
+cubby
+US ['kʌbɪ]
+UK ['kʌbɪ]
+n.整齐的场所[小房间]；狭窄的房间；分类架；〈俚〉【军】小壕沟(等)
+Web包厢服务；存包店；卡比多功能收纳盒
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- develop_enviroment/envsetup_modified.sh
+++ develop_enviroment/envsetup_modified.sh
@@ -1,45 +1,212 @@
-cat <<EOF
-说明：这里的函数定义只在原有命令的基础上增加了一些过滤条件，保持原有grep、find的语法，使用方法和原有grep、find一样。
-例：
-$ cgrep thename
-$ cgrep -i thename
-$ cgrep -E “\<thename\>”
-$ cgrep -i -E “\<thename\>”
-$ ffind -name thename -type f
-EOF
+#!/bin/bash
+#说明：这里的函数定义只在原有命令的基础上增加了一些过滤条件，保持原有grep、find的语法，使用方法和原有grep、find一样。
+#例：
+# cgrep thename
+# cgrep -i thename
+# cgrep -E “\<thename\>”
+# cgrep -i -E “\<thename\>”
+# ffind -name thename -type f
 
 #版本一：使用exec，注意末尾加'{} +'
 function cgrep()
 {
-    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name '*.o' -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
+    find . -name 'work_dir*' -prune -o -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name '*.o' -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
         -exec grep -a --color -n "$@" {} +
 }
 #版本二：使用xargs，注意'| xargs grep'的前面要假'-print'
 function cgrep2()
 {
-    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name '*.o' -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
+    find . -name 'work_dir*' -prune -o -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name '*.o' -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
         -print | xargs grep -a --color -n "$@"
 }
 #经测试，速度上-exec command {} +最快，-print|xargs command次之，-exec command {} \;最慢。20210822
 
 function agrep()
 {
-    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name '*.o' -prune -o -type f -exec grep -a --color -n "$@" {} +
+    find . -name 'work_dir*' -prune -o -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name '*.o' -prune -o -type f -exec grep -a --color -n "$@" {} +
 }
 
 function nocgrep()
 {
-    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name '*.o' -prune -o -name '*.c' -prune -o -name '*.cc' -prune -o -name '*.cpp' -prune -o -name '*.h' -prune -o -name '*.hpp' -prune -o -type f \
+    find . -name 'work_dir*' -prune -o -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name '*.o' -prune -o -name '*.c' -prune -o -name '*.cc' -prune -o -name '*.cpp' -prune -o -name '*.h' -prune -o -name '*.hpp' -prune -o -type f \
         -exec grep -a --color -n "$@" {} +
 }
 
 function mgrep()
 {
-    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name '*.o' -prune -o \( -regextype posix-egrep -iregex '(.*\/Makefile|.*\/Makefile\..*|.*\.make|.*\.mak|.*\.mk|.*\.bp)' -o -regextype posix-extended -regex '(.*/)?soong/[^/]*.go' \) -type f \
+    find . -name 'work_dir*' -prune -o -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name '*.o' -prune -o \( -regextype posix-egrep -iregex '(.*\/Makefile|.*\/Makefile\..*|.*\.make|.*\.mak|.*\.mk|.*\.bp)' -o -regextype posix-extended -regex '(.*/)?soong/[^/]*.go' \) -type f \
         -exec grep -a --color -n "$@" {} +
 }
 
 function ffind()
 {
+    find . -name 'work_dir*' -prune -o -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o "$@" -print
+}
+
+
+
+# 包含work_dir搜索
+function wcgrep()
+{
+    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name '*.o' -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
+        -exec grep -a --color -n "$@" {} +
+}
+
+function wagrep()
+{
+    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name '*.o' -prune -o -type f -exec grep -a --color -n "$@" {} +
+}
+
+function wnocgrep()
+{
+    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name '*.o' -prune -o -name '*.c' -prune -o -name '*.cc' -prune -o -name '*.cpp' -prune -o -name '*.h' -prune -o -name '*.hpp' -prune -o -type f \
+        -exec grep -a --color -n "$@" {} +
+}
+
+function wmgrep()
+{
+    find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o -name '*.o' -prune -o \( -regextype posix-egrep -iregex '(.*\/Makefile|.*\/Makefile\..*|.*\.make|.*\.mak|.*\.mk|.*\.bp)' -o -regextype posix-extended -regex '(.*/)?soong/[^/]*.go' \) -type f \
+        -exec grep -a --color -n "$@" {} +
+}
+
+function wffind()
+{
     find . -name .repo -prune -o -name .git -prune -o -name .svn -prune -o -name out -prune -o "$@" -print
+}
+
+function m3sync(){
+    echo "cp ../UGW_main/work_dir.bcm6756_11ax_nor_mesh3.0/vendor/release/bcm6756_11ax_nor_mesh3.0_release.bz2 platform"
+          cp ../UGW_main/work_dir.bcm6756_11ax_nor_mesh3.0/vendor/release/bcm6756_11ax_nor_mesh3.0_release.bz2 platform
+    echo "cp ../UGW_main/work_dir.bcm6756_11ax_nor_mesh3.0/cbb/wifi/bcm6756_11ax_nor_mesh3.0_wifi_release.bz2 platform"
+          cp ../UGW_main/work_dir.bcm6756_11ax_nor_mesh3.0/cbb/wifi/bcm6756_11ax_nor_mesh3.0_wifi_release.bz2 platform
+}
+
+
+function m2sync(){
+    echo "cp ../UGW_main/work_dir.bcm6756_11ax_nor/vendor/release/bcm6756_11ax_nor_release.bz2 platform"
+    cp ../UGW_main/work_dir.bcm6756_11ax_nor/vendor/release/bcm6756_11ax_nor_release.bz2 platform
+    echo "cp ../UGW_main/work_dir.bcm6756_11ax_nor/cbb/wifi/bcm6756_11ax_nor_wifi_release.bz2 platform"
+    cp ../UGW_main/work_dir.bcm6756_11ax_nor/cbb/wifi/bcm6756_11ax_nor_wifi_release.bz2 platform
+}
+
+
+function repo2()
+{
+    if [ "$1" != "sync" ] && [ "$1" != "status" ] && [ "$1" != "branch" ] && [ "$1" != "checkout" ] && [ "$1" != "start" ] && [ "$1" != "pull" ] && [ "$1" != "log" ] && [ "$1" != "rebase" ]; then
+        echo "command NOT support!!!"
+        return 1
+	fi
+
+    curdir=`pwd`
+    oldpwd=$OLDPWD
+
+    while true; do
+        if [ -f PLATFORM_TOP.flag ]; then
+            PROJ_TOP=`pwd`
+            break
+        else
+            cd ..
+            if [ "`pwd`" == "/" ]; then
+                echo "NOT in a UGW_main project repository !!!"
+                cd $curdir
+                OLDPWD=$oldpwd
+				unset PROJ_TOP
+                return 2
+            fi
+        fi
+    done
+
+
+    auto_chan_bw_select="cbb/wifi/auto_chan_bw_select"
+    mesh3="cbb/mesh3.0"
+    Tenda_Easymesh="cbb/mesh/Tenda_Easymesh"
+    Tenda_Xmesh="cbb/mesh/Tenda_Xmesh"
+    vendor="vendor"
+    subcmd=$*
+    only_cur_dir=0
+    cmd_not_support=0
+
+    if [ "$1" == "sync" ]; then
+        subcmd="pull --rebase"
+    fi
+
+    if [ "$1" == "start" ]; then
+        if [ "$#" -ne 2 ] && [ "$#" -ne 3 ]; then
+            echo "syntax error !!! input param num=$#"
+            return 3
+        else
+            remote_br=`git branch -r | grep HEAD | awk '{print $3}'`
+            subcmd="checkout -b $2 $remote_br"
+
+            if [ "$#" -eq 2 ]; then
+                only_cur_dir=1
+            fi
+
+            if [ "$#" -eq 3 ] && [ "$3" != "--all" ]; then
+                echo "syntax error !!! input param 3 error"
+                return 4
+            fi
+
+        fi
+    fi
+
+    if [ "$1" == "log" ];then
+        if [ "$#" -eq 1 -o "$#" -eq 2 ];then
+            if [ "$#" -eq 2 ]; then
+                if [ "$2" != "-1" ] && [ "$2" != "-2" ]; then
+                    echo "only 1 or 2 commit log support !!!"
+                    return 5
+                fi
+            else
+                subcmd="log -1"
+            fi
+        else
+            echo "too many input paramters !!!"
+            return 6
+        fi
+    fi
+
+
+    if [ $only_cur_dir == 1 ]; then
+        git $subcmd
+        echo
+    else
+        echo $PROJ_TOP/$auto_chan_bw_select
+        cd $PROJ_TOP/$auto_chan_bw_select
+        git $subcmd
+        echo
+
+        echo $PROJ_TOP/$mesh3
+        cd $PROJ_TOP/$mesh3
+        git $subcmd
+        echo
+
+        echo $PROJ_TOP/$Tenda_Easymesh
+        cd $PROJ_TOP/$Tenda_Easymesh
+        git $subcmd
+        echo
+
+        echo $PROJ_TOP/$Tenda_Xmesh
+        cd $PROJ_TOP/$Tenda_Xmesh
+        git $subcmd
+        echo
+
+        echo $PROJ_TOP/$vendor
+        cd $PROJ_TOP/$vendor
+        git $subcmd
+        echo
+
+        echo $PROJ_TOP
+        cd $PROJ_TOP
+        git $subcmd
+        echo
+
+        cd $curdir
+        OLDPWD=$oldpwd
+    fi
+
+    unset subcmd
+    unset remote_br
+    unset PROJ_TOP
+    unset curdir
 }
