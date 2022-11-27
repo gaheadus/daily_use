18d2ce140ea5c56f9ab9f11a5b6e6b1201291641
dukang <gaheadus@126.com>
2022-11-27 17:00:18 +0800

    修改，新增

---
+++ English/word_fc.c
@@ -0,0 +1,43 @@
+/* 英语单词，来源于微软必应词典、百度翻译。
+author: dukang, gaheadus@126.com, 20220516  */
+
+
+lebal:
+约定：
+单词写成C语言'函数'或'标签'的形式。写成'函数'好看一些。
+单词名写成函数名，读音、释义、例句等都放到单引号或双引号里面，例'example'、"example 2"。放双引号里看起来舒服些。
+文字外面加双引号放在任何地方都可以，"this is an example 3"，整体就是一个字符串。
+注意：读音重音符号用ˈ，不用单引号'，否则sourceinsight不能正确解析函数定义'。
+优点：
+1)极大的方便单词查找，方便学习。通过sourceinsight的符号窗口快速定位到单词所在位置。word文档、记事本等不能搜索目录文字，不能快速定位。
+2)sourceinsight将函数名加粗突出显示，方便阅读。
+3)通过符号窗口搜索，可以找出相近单词、重复单词。
+4)例句里面的生词，可以在附近位置写成函数调用，这样可以快速跳转到单词释义所在位置，结合sourceinsight快捷键前进后退用起来非常舒服，例如:verdict();
+非常好用，这个方法值得推广。
+如果朗文词典/牛津词典能按照这个方式编写也不错，比一页一页翻着查找速度快多了，哈哈哈。这样能在词典里做笔记，并不影响词典原有内容，不错。
+
+
+
+
+todo:
+如下是easymesh相关单词，主要来自协议标准文档。
+
+
+prise(){ " 美 [praɪz] 英 [praɪz]
+           v.强行使分开；撬开 to use force to separate sth from sth else
+           [网络]杠杆；奖赏；按价格  "}
+reprise(){ "美 [rɪˈpriz] 英 [rɪˈpriːz]
+              v.重演；〈古〉夺回；〈古〉赔偿
+              n.重复部分；（尤指乐曲的）反复 a repeated part of sth, especially a piece of music
+              [网络]重奏；年金；巴黎塔尖 "}
+
+avionics(){ "美 [ˌeɪviˈɑnɪks] 英 [ˌeɪviˈɒnɪks] //只有复数，没有单数
+              n.航空电子学；航空（或航天）电子设备
+              [网络]航电；航空电子设备；航电设备
+              1.[u]航空电子学the science of electronics when used in designing and making aircraft
+              2.[pl]航空（或航天）电子设备the electronic devices in an aircraft or a spacecraft " }
+
+cavium(){ "凯为，半导体公司名字。
+             Cavium Inc 公司是全球领先的多核 MIPS 和 ARM 处理器提供商，处理器广泛应用于网络/通讯、无线、存储和控制应用等领域的安全产品。" }
+
+
